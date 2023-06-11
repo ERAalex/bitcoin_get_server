fca9939b481da23392ea31dd177c6877a8ef2dee
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-06-11 16:41:47 +0300

    Done ascheduler work getting api data from Deribir every 60 seconds

---
+++ scheduler_work.py
@@ -0,0 +1,18 @@
+import asyncio
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from deribit_api import call_api
+
+async def main():
+    # Init message
+    print('\nPress Ctrl-C to quit at anytime!\n')
+
+    scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
+    # scheduler.add_job(foo, trigger='interval', seconds=1)
+    scheduler.add_job(call_api, trigger='interval', seconds=5)
+    scheduler.start()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
